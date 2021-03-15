bcb57fe7f873d18bdca2ef2e58c9ba331a7b79dd
nic0lasfara <nicolas.farabegoli@gmail.com>
2021-03-15 12:46:24 +0100

    Finish fibonacci stream

--- src/u03/Streams.scala
+++ src/u03/Streams.scala
@@ -1,6 +1,9 @@
 package u03
 
 import u03.Streams.Stream
+import u03.Streams.Stream.{constant, fibs}
+
+import scala.annotation.tailrec
 
 object Streams {
   import Lists._
@@ -40,6 +43,26 @@ object Streams {
     }
 
     def iterate[A](init: => A)(next: A => A): Stream[A] = cons(init, iterate(next(init))(next))
+
+    @tailrec
+    def drop[A](stream: Stream[A])(n: Int): Stream[A] = stream match {
+      case Cons(_, tail) if n > 1 => drop(tail())(n-1)
+      case Cons(_, tail) => tail()
+      case _ => Empty()
+    }
+
+    def constant[A](value: => A): Stream[A] = cons(value, constant(value))
+
+    @tailrec
+    def foldLef[A](stream: Stream[A])(initial: A)(op: (A, A) => A): A = stream match {
+      case Cons(head, tail) => foldLef(tail())(op(initial, head()))(op)
+      case _ => initial
+    }
+
+    val fibs: Stream[Int] = {
+      def _fib(v1: => Int, v2: => Int): Stream[Int] = cons[Int](v1, _fib(v2, v1 + v2))
+      _fib(0, 1)
+    }
   }
 }
 
@@ -53,4 +76,11 @@ object StreamsMain extends App {
 
   val corec: Stream[Int] = Stream.cons(1, corec) // {1,1,1,..}
   println(Stream.toList(Stream.take(corec)(10))) // [1,1,..,1]
+
+  val s = Stream.take(Stream.iterate(0)(_+1))(10)
+  println(Stream.toList(Stream.drop(s)(6))) // => Cons(6,Cons(7,Cons(8,Cons(9,Nil()))))
+
+  println(Stream.toList(Stream.take(constant("x"))(5)))
+
+  println(Stream.toList(Stream.take(fibs)(8)))
 }
