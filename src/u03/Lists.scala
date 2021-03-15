e545c43c4da6dcb5392096a9bbb8300ceb3f093a
nic0lasfara <nicolas.farabegoli@gmail.com>
2021-03-15 11:38:24 +0100

    Finish flatMap and other. Finish max

--- src/u03/Lists.scala
+++ src/u03/Lists.scala
@@ -1,6 +1,7 @@
 package u03
 
 import scala.annotation.tailrec
+import u02.Optionals._
 
 object Lists {
 
@@ -28,7 +29,7 @@ object Lists {
     }
 
     def filter[A](l1: List[A])(pred: A=>Boolean): List[A] = l1 match {
-      case Cons(h,t) if (pred(h)) => Cons(h, filter(t)(pred))
+      case Cons(h,t) if pred(h) => Cons(h, filter(t)(pred))
       case Cons(_,t) => filter(t)(pred)
       case Nil() => Nil()
     }
@@ -38,6 +39,32 @@ object Lists {
       case Cons(_, tail) if n > 0 => drop(tail, n - 1)
       case Cons(head, tail) => Cons(head, tail)
       case _ => Nil()
+    }
+
+    def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = l match {
+      case Cons(head, tail) => append(f(head), flatMap(tail)(f))
+      case _ => Nil()
+    }
+
+    def mapFlatMap[A, B](l: List[A])(mapper: A => B): List[B] = flatMap(l)(e => Cons(mapper(e), Nil()))
+
+    def filterFlatMap[A, B](l: List[A])(pred: A => Boolean): List[A] = flatMap(l) {
+      case e if pred(e) => Cons(e, Nil())
+      case _ => Nil()
+    }
+
+    def max(l: List[Int]): Option[Int] = {
+      @tailrec
+      def _max(list: List[Int], currMax: Int): Int = list match {
+        case Cons(head, tail) if head > currMax => _max(tail, head)
+        case Cons(head, tail) if head <= currMax => _max(tail, currMax)
+        case Nil() => currMax
+      }
+
+      l match {
+        case Cons(_, _) => Option.Some(_max(l, Int.MinValue))
+        case Nil() => Option.None()
+      }
     }
   }
 }
@@ -54,4 +81,12 @@ object ListsMain extends App {
   println(drop(l, 1)) // 20,30
   println(drop(l, 2)) // 30
   println(drop(l, 3)) // Nil()
+
+  println(flatMap(l)(v => Cons(v+1, Nil()))) //11, 21, 31
+
+  println(mapFlatMap(l)(_ + 1)) // 11, 21, 31
+  println(filterFlatMap(l)(_ >= 20)) // 20, 30
+
+  println(max(l))
+  println(max(Nil[Int]()))
 }
